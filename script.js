d40facdf964e37963bfe2db8a685aad805ac9bee
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-08-05 16:13:42 +0800

    added sekeleton and some css

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert('test')
+const personFactory = (name, age) => {
+    const sayHello = () => console.log('hello!');
+    return { name, age, sayHello };
+  };
+  
+  const jeff = personFactory('jeff', 27);
+  
+  console.log(jeff.name); // 'jeff'
+  
+  jeff.sayHello(); // calls the function and logs 'hello!'
+  //Factory function
+
+  //if you only ever need ONE of something (gameBoard, displayController), use a module. 
+  //If you need multiples of something (players!), create them with factories.
+  const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+      add,
+      sub,
+      mul,
+      div,
+    };
+  })();
+  
+  calculator.add(3,5); // 8
+  calculator.sub(6,2); // 4
+  calculator.mul(14,5534); // 77476
+//module    
